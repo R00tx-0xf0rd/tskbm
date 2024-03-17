4bc37b06763c1de88fc551e34608082c0be485ec
SA <55909root@gmail.com>
2024-03-17 15:26:44 +0900

    delete another, and add fresh files

---
+++ db/__init__.py
@@ -0,0 +1,2 @@
+from .database import new_session, create_tables, delete_tables
+from .models.tables import Users
--- tskbm/users/crud.py
+++
@@ -1,6 +0,0 @@
-from tskbm.users.models import CreateUser
-
-
-def create_user(user_in: CreateUser):
-    user = user_in.model_dump()
-    return {"operation": "success", "user": user}
