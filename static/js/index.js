820f9c187e3a3c94aca9bb2d9872498b70e1dc04
R00tx-0xf0rd <PHNg2F58bGRu@eR>
2024-03-27 10:40:58 +0300

    add new functions

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,62 @@
-function redirect()
-{
-console.log('clicked');
-window.location='/'
+function redirect() {
+  console.log("clicked");
+  window.location = "/";
 }
-
 
 function doFunction() {
-setTimeout(redirect, 5000);
+  setTimeout(redirect, 5000);
 }
-
 
 function process() {
-if (confirm('Хотите добавить срок поверки ?')) {
-  // Save it!
-  console.log('Thing was saved to the database.');
-} else {
-  // Do nothing!
-  console.log('Thing was not saved to the database.');
+  if (confirm("Хотите добавить срок поверки ?")) {
+    // Save it!
+    console.log("Thing was saved to the database.");
+  } else {
+    // Do nothing!
+    console.log("Thing was not saved to the database.");
+  }
 }
+
+async function get_result(url) {
+  fetch(url)
+    .then((resp) => {
+      if (!resp.ok) {
+        throw Error(`is not ok: ` + resp.status);
+      }
+      return resp.json();
+    })
+    .catch((err) => {
+      console.warn(err);
+    });
+}
+
+async function get_periods() {
+  let tabnum = document.querySelector("#checkout_tn").value;
+
+  console.log(tabnum);
+  element = document.querySelector(".answer");
+  let url = "/users/" + tabnum + "/";
+
+  let result = await fetch(url).then((resp) => resp.json());
+
+  if (!result) {
+    element.innerText = "Не найдено";
+  } else {
+    console.log(result);
+    element.innerText = result.lname;
+  }
+
+  // let user = NaN;
+  // if (response.ok) {
+  //   console.log("ok");
+  //   // если HTTP-статус в диапазоне 200-299
+  //   // получаем тело ответа (см. про этот метод ниже)
+  //   user = await response.json();
+  // }
+  // if (user != isNaN) {
+
+  // }
+
+  // console.log(user);
+  // document.location = "/";
 }
